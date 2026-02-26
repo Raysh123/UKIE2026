7a40d1736d2ae8fbc25126462b2190e5b9eeb755
Aaron Hopkins <aaronpatrickhopkins@gmail.com>
2026-02-26 19:25:39 +0000

    linked scens on UI, stop and restart audio on pause

--- Assets/Scripts/Pause Menu.cs
+++ Assets/Scripts/Pause Menu.cs
@@ -6,6 +6,7 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public AudioSource audioSource;
 
     private void Update()
     {
@@ -26,6 +27,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        audioSource.Play();
     }
 
     private void Pause()
@@ -33,6 +35,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        audioSource.Stop();
     }
 
     public void LoadMenu()

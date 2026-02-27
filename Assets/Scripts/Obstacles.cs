56d51da4141e9b267c07791799ba06a0a612b01a
Raysh123 <111732980+Raysh123@users.noreply.github.com>
2026-02-27 13:35:10 +0000

    Fixed Shield

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour
 {
+    public SpiritMovement spiritMovement;
     public Health health;
     public int damage = 1;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Health health = collision.GetComponent<Health>();
+        spiritMovement = collision.GetComponent<SpiritMovement>();
         if (health != null)
         {
-            health.TakeDamage(damage); 
+            {
+                if(spiritMovement.shieldActive)
+                {
+                    spiritMovement.shieldActive = false;
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    health.TakeDamage(damage); 
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
